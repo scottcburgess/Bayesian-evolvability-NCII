c923cdd106e49da83bf98d7d8cdeca8a66603fb1
ericarcher <eric.ivan.archer@gmail.com>
2025-10-01 08:45:37 -0700

    increase maximum of variance priors in Model II

--- Model_II.R
+++ Model_II.R
@@ -49,10 +49,10 @@ post <- run.jags(
     interaction.mean ~ dnorm(0, 1e-3)
       
     # ---- variance priors ----
-    sire.var ~ dunif(0, 1e2)
-    dam.var ~ dunif(0, 1e2)
-    interaction.var ~ dunif(0, 1e2)
-    resid.var ~ dunif(0, 1e2)
+    sire.var ~ dunif(0, 1e4)
+    dam.var ~ dunif(0, 1e4)
+    interaction.var ~ dunif(0, 1e4)
+    resid.var ~ dunif(0, 1e4)
   
     # ---- sire priors ----
     for(s in 1:n.sires) {
